4f9e5f2808f9370842f91b30c0f28f2193a954e0
annpkn <ann20pekun@example.com>
2021-12-18 23:10:56 +0300

    create hw5

--- homework5/parents.js
+++ homework5/parents.js
@@ -1,1 +1,7 @@
-function showAlert()
+function showAlertWithParentsData() {
+    const momName = document.getElementById("name").value;
+    const dadName = document.getElementById("name2").value;
+    const result = `${momName},${dadName}`;
+    alert(result);
+
+}
